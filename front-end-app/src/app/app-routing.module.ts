dfdb520f8b3f10b81a02c27ef1cd8be6c69e5d1c
Matthieu d'Hoop <matthieu.dhoop@gmail.com>
2023-10-20 17:47:14 +0200

    config files added

--- front-end-app/src/app/app-routing.module.ts
+++ front-end-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AccueilComponent},
+  { path: 'test', component: TestComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
