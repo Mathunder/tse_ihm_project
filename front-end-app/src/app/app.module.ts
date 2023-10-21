c1754033d6825c599c1b52154929a6df42f34adf
Matthieu d'Hoop <matthieu.dhoop@gmail.com>
2023-10-21 11:17:17 +0200

    product data added

--- front-end-app/src/app/app.module.ts
+++ front-end-app/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponentComponent } from './test-component/test-component.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { HeaderComponent } from './header/header.component';
 import { ProductsListComponent } from './products-list/products-list.component';
@@ -14,7 +13,6 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponentComponent,
     AccueilComponent,
     HeaderComponent,
     ProductsListComponent,
---
+++ front-end-app/src/interfaces/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+	name: string,
+	marque: string,
+	id: number,
+	category: string,
+	price: number,
+	currency: string
+}
